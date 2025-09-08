a70d4f5b32271109549daf9b2ecfc3c4906bfdff
Ivy <ivyhuangamazing@gmail.com>
2025-09-08 18:25:36 -0400

    finished multiples file

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(10, 3, 5));
+    }
+
+   static int multiples(int n, int a, int b) {
         int count = 0;
-        for (int i = 1; i < 1000; i++) {
-            boolean divBy3 = i % 3 == 0;
-            boolean divBy5 = i % 5 == 0;
+        for (int i = 1; i < n; i++) {
+            boolean divBy3 = i % a == 0;
+            boolean divBy5 = i % b == 0;
             if (divBy3 || divBy5) {
                 count += 1;
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
